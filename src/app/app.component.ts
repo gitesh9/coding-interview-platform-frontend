e5665ffcf91a5112b0c3d7eb5367f964bfb620e6
Gitesh Wankhede <gitesh@Giteshs-MacBook-Pro.local>
2026-02-18 00:12:12 +0530

    updated sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite'
 import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navigation/sidebar/sidebar.component.ts
+++ src/app/navigation/sidebar/sidebar.component.ts
@@ -1,25 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostBinding } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NavigationService } from '../../navigation.service';
+import { RouterModule } from '@angular/router';
+import { Drawer } from 'flowbite';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-  isHidden = false;
+export class SidebarComponent implements OnInit {
+  isOpen = false;
+  @ViewChild("allProblemsLink",{static:false}) allProblemsLink!: ElementRef;
+  @ViewChild("drawer",{static:false}) drawer!: ElementRef;
 
-  toggle() {
-    console.log("here:")
-    this.isHidden = !this.isHidden;
+  constructor(private stateService: NavigationService) {}
+
+  ngOnInit() {
+    this.stateService.sidebarOpen$.subscribe(open => {
+      this.isOpen = open;
+      if (open) {
+        console.log(open)
+        // Optional: Add focus logic here
+        setTimeout(() => {
+          console.log("opened.",this.allProblemsLink.nativeElement)
+          this.allProblemsLink.nativeElement?.focus();
+        }, 100);
+      }
+    });
   }
 
-  hide() {
-    this.isHidden = false;
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const button = document.querySelector('#sidebar-button');
+    const clickedButton = button?.contains(event.target as Node)
+    if (this.isOpen && this.drawer && !this.drawer.nativeElement.contains(event.target) && !clickedButton) {
+      this.closeSidebar();
+    }
   }
-
-  show() {
-    this.isHidden = true;
+  closeSidebar() {
+    this.stateService.setSidebarOpen(false);
   }
 }
