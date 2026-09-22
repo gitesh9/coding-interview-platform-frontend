5eaffee85a9965e0235b2d1788b7c92d9c8a500d
Gitesh <giteshwan98@gmail.com>
2026-09-22 23:22:51 +0530

    feat: add a dropdown and user initials instead of a avatar

--- src/app/shared/user/user.component.ts
+++ src/app/shared/user/user.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '@core/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  private authService = inject(AuthService);
 
+  currentUser = this.authService.currentUser;
+  userInitials = computed(() => {
+    const name = this.currentUser()?.name?.trim();
+    if (!name) return 'U';
+
+    const nameParts = name.split(/\s+/);
+    const firstInitial = nameParts[0]?.[0] ?? '';
+    const lastInitial =
+      nameParts.length > 1 ? (nameParts[nameParts.length - 1]?.[0] ?? '') : '';
+
+    return `${firstInitial}${lastInitial}`.toUpperCase();
+  });
 }
